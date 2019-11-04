1937761a848667369bc2fd6c6d0bd345ae77737d
Sashok1188 <Sasha-uchitel@mail.ru>
2019-11-04 13:47:55 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double i, x = 0;
+
+
+            for (i = -21; i <= -5; i++)
+            {
+                sum += i;
+                x++;
+            }
+            Console.WriteLine(sum / x);
+
+        }
+    }
+}
